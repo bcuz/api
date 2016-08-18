18beeacc6299e500051c7449c0f84a9029a1f048
Adam Nawrocki <adam.nawroc@gmail.com>
2016-08-18 07:46:09 -0400

    just dup the existing call

--- js/app.js
+++ js/app.js
@@ -130,38 +130,43 @@ var getUnanswered = function() {
 	});
 };
 
-var topPeeps = function(tag) {
-	tagged_url = 'https://api.stackexchange.com/2.2/tags/' + tag + '/top-answerers/all_time'
-		// the parameters we need to pass in our request to StackOverflow's API
-	var request = {
-		// tagged: tags,
-		site: 'stackoverflow'
-		// order: 'desc',
-		// sort: 'creation'
-	};
+var getUnanswered = function() {
+
+	// the parameters we need to pass in our request to StackOverflow's API
+	// var request = {
+	// 	tagged: tags,
+	// 	site: 'stackoverflow',
+	// 	order: 'desc',
+	// 	sort: 'creation'
+	// };
+
 	$.ajax({
-		url: tagged_url,
-		data: request,
+		url: "https://api.wunderground.com/api/be93c4c3fa5780ba/geolookup/conditions/q/MI/Warren.json",
+		// data: request,
 		dataType: "jsonp",//use jsonp to avoid cross origin issues
-		type: "GET",
+		type: "GET"
 	})
 	.done(function(result){ //this waits for the ajax to return with a succesful promise object
-		console.log(result.items);
-		var searchResults = showSearchResults(tag, result.items.length);
-
-		$('.search-results').html(searchResults);
+		console.log(result);
+		// var searchResults = showSearchResults(request.tagged, result.items.length);
+		// $('.search-results').html(searchResults);
 		// //$.each is a higher order function. It takes an array and a function as an argument.
 		// //The function is executed once for each item in the array.
-		$.each(result.items, function(i, item) {
-			var answer = showAnswerers(item);
-			$('.results').append(answer);
-		});
+		// $.each(result.items, function(i, item) {
+		// 	var question = showQuestion(item);
+		// 	$('.results').append(question);
+
+		var answer = showWeather(result)
+		$("body").prepend(answer)
+
+
 	})
 	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
-		var errorElem = showError(error);
-		$('.search-results').append(errorElem);
+		console.log(error);
 	});
-}
+};
+
+
 
 	getUnanswered();
 });
